363129de57cd0a8719890d7655673ac60b74d04e
Elvin Augusto García Camizán <egarciagc@hotmail.com>
2019-09-22 05:01:52 -0500

    Actualixado hasta el vídeo Parte 09

---
+++ WEB_PROYECTOS/Scripts/app.js
@@ -0,0 +1,21 @@
+﻿function LoadingOverlayShow(id) {
+    $(id).LoadingOverlay("show", {
+        color: "rgba(255, 255, 255, 0.5)",
+        image: "/Content/loading.gif",
+        imageResizeFactor: 0.6,
+        //imageAnimation: "1.5s fadein",
+    });
+}
+
+function LoadingOverlayHide(id) {
+    $(id).LoadingOverlay("hide");
+}
+
+function ValidarFechas(dateIni, dateFin) {
+    var _dateIni = new Date(dateIni);
+    var _dateFin = new Date(dateFin);
+    if (_dateFin < _dateIni)
+        return false;
+    else
+        return true;
+} 
